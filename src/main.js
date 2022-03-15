9041ae48aa8c098c59da35ab148575aa43b7219e
Chen Cheng <602872445@qq.com>
2022-03-15 20:12:32 +0800

    增加vuex状态管理

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import router from "./router";
 import vant from "vant";
 import "vant/lib/index.css";
 import instance from "./util";
+import store from "./store";
 Vue.use(vant);
 Vue.config.productionTip = false;
 new Vue({
   render: (h) => h(App),
   router,
+  store,
+  created() {
+    console.log(this.$store);
+  },
 }).$mount("#app");
 Vue.prototype.$axios = instance;
--- src/pages/Find/components/RankList.vue
+++ src/pages/Find/components/RankList.vue
@@ -15,7 +15,16 @@ export default {
 
   methods: {},
   created() {
-    this.$axios.get("/api/toplist/detail").then((res) => {
+    /* fetch(
+      "http://192.168.1.98:3000/login/cellphone?phone=17634428483&password=cc20010227"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      }); */
+    this.$axios.get("/api/toplist").then((res) => {
       console.log("排行榜", res);
     });
   },
