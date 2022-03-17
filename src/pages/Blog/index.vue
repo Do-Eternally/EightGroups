86dc13737237b94b5f01f61b68926bc7cc4c9e07
lisi <1466219134@qq.com>
2022-03-17 11:31:40 +0800

    pull router

--- src/pages/Blog/index.vue
+++ src/pages/Blog/index.vue
@@ -15,27 +15,34 @@
 
     <van-swipe
       class="my-swipe"
-      :autoplay="false"
+      autoplay="false"
       indicator-color="white"
       :show-indicators="false"
     >
       <van-swipe-item>
         <van-row gutter="20">
-          <div></div>
-          <van-col span="8" class="w"></van-col>
-
-          <van-col span="8" class="w"></van-col>
-          <van-col span="8" class="w"></van-col>
-          <van-col span="8" class="w"></van-col>
-
-          <!-- <van-col span="8">span: 8</van-col>
-  <van-col span="8">span: 8</van-col>
-  <van-col span="8">span: 8</van-col> -->
+          <van-col span="5" class="w"
+            ><van-icon name="fire" size="70" color="red" />
+          </van-col>
+          <van-col span="5" class="w">
+            <van-icon name="service " size="70" color="red" />
+          </van-col>
+          <van-col span="5" class="w">
+            <van-icon name="medal" size="70" color="red" />
+          </van-col>
+          <van-col span="5" class="w">
+            <van-icon name="like" size="70" color="red" />
+          </van-col>
+          <van-col span="5" class="w">
+            <van-icon name="photograph" size="70" color="red"
+          /></van-col>
         </van-row>
       </van-swipe-item>
       <van-swipe-item>
         <van-row gutter="20">
-          <van-col span="8" class="w"></van-col>
+          <van-col span="5" class="w">
+            <van-icon name="point-gift" size="70" color="red" />
+          </van-col>
 
           <!-- <van-col span="8">span: 8</van-col>
   <van-col span="8">span: 8</van-col>
@@ -46,60 +53,64 @@
 
     <div class="d1">
       <van-swipe :loop="false" :width="300" :show-indicators="false" class="q">
-        <van-swipe-item class="qq"></van-swipe-item>
-        <van-swipe-item class="q1"></van-swipe-item>
-        <van-swipe-item class="qq"></van-swipe-item>
-        <van-swipe-item class="q1"></van-swipe-item>
+        <van-swipe-item
+          class="qq"
+          v-for="item in obj"
+          :key="item.id + Math.random()"
+        >
+          <p>{{ item.name }}</p>
+
+          <img :src="item.picUrl" alt="" />
+        </van-swipe-item>
       </van-swipe>
     </div>
     <van-tabs v-model="active">
-      <van-tab title="推荐">
+      <van-tab title="推荐" class="r1">
+        <van-row
+          gutter="20"
+          class="r"
+          v-for="item in ss"
+          :key="item.id"
+          @click="to(item.id)"
+        >
+          <van-col offset="6" span="5" class="c1">
+            <img :src="item.picUrl" alt="" class="m1" />
+            <p>{{ item.name }}</p>
+          </van-col>
+          <!-- <van-col span="5" class="c1">span: 8</van-col>
+          <van-col span="6" class="c1">span: 8</van-col> -->
+        </van-row>
+
+        <!-- <van-row gutter="20" class="r">
+          <van-col offset="4" span="5" class="c1">span: 8</van-col>
+          <van-col span="5" class="c1">span: 8</van-col>
+          <van-col span="6" class="c1">span: 8</van-col>
+        </van-row>
+
         <van-row gutter="20" class="r">
-          <van-col offset="6" span="5" class="c1">span: 8</van-col>
-          <van-col span="5" class="c1">span: 8</van-col>
-          <van-col span="6" class="c1">span: 8</van-col>
-        </van-row>
-
-
-          <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
-        </van-row>
-
-
-
-          <van-row gutter="20" class="r">
-          <van-col offset="4" span="5" class="c1">span: 8</van-col>
-          <van-col span="5" class="c1">span: 8</van-col>
-          <van-col span="6" class="c1">span: 8</van-col>
-        </van-row>
+        </van-row> -->
       </van-tab>
       <van-tab title="真实故事">
         <van-row gutter="20" class="r">
-          <van-col offset="6" span="5" class="c1">span: 8</van-col>
-          <van-col span="5" class="c1">span: 8</van-col>
-          <van-col span="6" class="c1">span: 8</van-col>
+          <van-col offset="6" span="5" class="c1"> </van-col>
+          <!-- <van-col span="5" class="c1">span: 8</van-col>
+          <van-col span="6" class="c1">span: 8</van-col> -->
         </van-row>
 
-
-          <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
 
-
-
-          <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
-
-
-
-
       </van-tab>
       <van-tab title="每日必听">
         <van-row gutter="20" class="r">
@@ -108,38 +119,32 @@
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
 
-
-          <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
 
-
-
-          <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
       </van-tab>
       <van-tab title="听见好书">
-       <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="6" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
 
-
-          <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
         </van-row>
 
-
-
-          <van-row gutter="20" class="r">
+        <van-row gutter="20" class="r">
           <van-col offset="4" span="5" class="c1">span: 8</van-col>
           <van-col span="5" class="c1">span: 8</van-col>
           <van-col span="6" class="c1">span: 8</van-col>
@@ -154,6 +159,8 @@ import { Tab, Tabs } from "vant";
 import { Search } from "vant";
 import { Col, Row } from "vant";
 
+// import {reqProducts} from ''
+
 export default {
   name: "Bolg",
   data() {
@@ -161,13 +168,58 @@ export default {
       activeName: "b",
       value: "",
       active: 0,
+      obj: "",
+      cc: "",
+      ss: "",
+      xx: "",
     };
   },
   computed: {},
   watch: {},
 
-  methods: {},
-  created() {},
+  methods: {
+    to(id) {
+      console.log(id);
+      this.$router.push({
+        // path: "/find/songslist",
+        nameL: "SonglistDetail",
+        query: {
+          id,
+        },
+      });
+    },
+  },
+  created() {
+    this.$axios.get("/api/personalized/djprogram").then((res) => {
+      console.log(res, 111);
+      this.obj = res.result;
+      console.log(this.obj, 22222);
+    });
+
+    this.$axios.get("/api/dj/program/toplist?limit=5").then((res) => {
+      console.log(res, 888);
+      this.cc = res.toplist;
+      console.log(this.cc, 333);
+    });
+
+    this.$axios.get("/api/personalized").then((res) => {
+      console.log(res, 777);
+      this.ss = res.result;
+      // console.log(this.ss, 444);
+    });
+
+    this.$axios.get("/song/detail").then((res) => {
+      console.log(res, 666);
+      this.ss = res.result;
+      console.log(this.ss, 444);
+    });
+
+    // this.$axios.get("/api/program/recommend").then((res) => {
+    //   console.log(res, 111777);
+    //   this.ss = res.result;
+    //   console.log(this.xx, 444);
+    // });
+  },
   mounted() {},
   components: {},
 };
@@ -192,7 +244,7 @@ export default {
 .w {
   width: 24.333333%;
   border-radius: 49%;
-  background-color: tomato;
+  /* background-color: tomato; */
   height: 54px;
 }
 .d1 {
@@ -208,7 +260,7 @@ export default {
   height: 120px;
 }
 .q1 {
-  background-color: #f3f1f494;
+  /* background-color: red; */
   margin-top: 20px;
 }
 .s1 {
@@ -219,20 +271,46 @@ van-col span {
   display: block;
   width: 200px;
   height: 30px;
-  background-color: red;
+  /* background-color: red; */
 }
-.c1{
+.c1 {
   height: 100px;
-  background-color: red;
+
   margin-top: 8px;
   margin-left: 8px;
 }
-.r{
-  margin-left: 67px;
+.r {
+  /* margin-left: 67px; */
+
+  height: 200px;
+  width: 32%;
+  /* display: flex;
+  justify-content: space-around; */
 }
 
 .van-col--6 {
-    width: 21%;
+  width: 21%;
 }
-
+img {
+  width: 60px;
+  height: 60px;
+  /* float: left; */
+  margin-top: 20px;
+}
+p {
+  float: right;
+}
+.r1 {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.c1 {
+  width: 84.833333%;
+}
+.m1 {
+  width: 100px;
+  height: 100px;
+  border-radius: 100px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,95 +2,87 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
 const router = new VueRouter({
-    routes: [{
-            path: "/",
-            redirect: "/find",
+  routes: [
+    {
+      path: "/",
+      redirect: "/find",
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import("../pages/Login"),
+      meta: { showFooter: false },
+    },
+    {
+      path: "/reg",
+      name: "Reg",
+      component: () => import("../pages/Reg"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/find",
+      name: "Find",
+      component: () => import("../pages/Find"),
+      meta: { showFooter: true },
+      children: [
+        {
+          path: "songslist", //歌单
+          name: "SongsList",
+          component: () => import("../pages/Find/components/SongsList.vue"),
+          meta: { showFooter: true },
         },
         {
-            path: "/login",
-            name: "Login",
-            component: () =>
-                import ("../pages/Login"),
-            meta: { showFooter: false },
+          path: "recommended", //每日推荐
+          name: "RecommendedDaily",
+          component: () =>
+            import("../pages/Find/components/RecommendedDaily.vue"),
+          meta: { showFooter: true },
         },
         {
-            path: "/reg",
-            name: "Reg",
-            component: () =>
-                import ("../pages/Reg"),
-            meta: { showFooter: true },
+          path: "ranklist", //排行榜
+          name: "RankList",
+          component: () => import("../pages/Find/components/RankList"),
+          meta: { showFooter: true },
         },
         {
-            path: "/find",
-            name: "Find",
-            component: () =>
-                import ("../pages/Find"),
-            meta: { showFooter: true },
-            children: [{
-                    path: "songslist", //歌单
-                    name: "SongsList",
-                    component: () =>
-                        import ("../pages/Find/components/SongsList.vue"),
-                    meta: { showFooter: true },
-                },
-                {
-                    path: "recommended", //每日推荐
-                    name: "RecommendedDaily",
-                    component: () =>
-                        import ("../pages/Find/components/RecommendedDaily.vue"),
-                    meta: { showFooter: true },
-                },
-                {
-                    path: "ranklist", //排行榜
-                    name: "RankList",
-                    component: () =>
-                        import ("../pages/Find/components/RankList"),
-                    meta: { showFooter: true },
-                },
-                {
-                    path: "privatefm", //私人FM
-                    name: "PrivateFM",
-                    component: () =>
-                        import ("../pages/Find/components/PrivateFM"),
-                    meta: { showFooter: true },
-                },
-            ],
+          path: "privatefm", //私人FM
+          name: "PrivateFM",
+          component: () => import("../pages/Find/components/PrivateFM"),
+          meta: { showFooter: true },
         },
-        {
-            path: "/blog",
-            name: "Blog",
-            component: () =>
-                import ("../pages/Blog"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/mine",
-            name: "Mine",
-            component: () =>
-                import ("../pages/Mine"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/attention",
-            name: "Attention",
-            component: () =>
-                import ("../pages/Attention"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/country",
-            name: "Country",
-            component: () =>
-                import ("../pages/Country"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/sidebar",
-            name: "Sidebar",
-            component: () =>
-                import ("../pages/Sidebar"),
-            meta: { showFooter: false },
-        },
-    ],
+      ],
+    },
+    {
+      path: "/blog",
+      name: "Blog",
+      component: () => import("../pages/Blog"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/mine",
+      name: "Mine",
+      component: () => import("../pages/Mine"),
+      meta: { showFooter: true },
+    },
+
+    {
+      path: "/attention",
+      name: "Attention",
+      component: () => import("../pages/Attention"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/country",
+      name: "Country",
+      component: () => import("../pages/Country"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/sidebar",
+      name: "Sidebar",
+      component: () => import("../pages/Sidebar"),
+      meta: { showFooter: false },
+    },
+  ],
 });
 export default router;
