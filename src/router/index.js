3b203f6a2f0914bea6ae61320982a5a04b260179
Chen Cheng <602872445@qq.com>
2022-03-17 10:11:15 +0800

    排行榜、详细以及简单播放页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,99 +2,85 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
 const router = new VueRouter({
-    routes: [{
-            path: "/",
-            redirect: "/find",
+  routes: [
+    {
+      path: "/",
+      redirect: "/find",
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import("../pages/Login"),
+      meta: { showFooter: false },
+    },
+    {
+      path: "/reg",
+      name: "Reg",
+      component: () => import("../pages/Reg"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/find",
+      name: "Find",
+      component: () => import("../pages/Find"),
+      meta: { showFooter: true },
+      children: [
+        {
+          path: "songslist", //歌单
+          name: "SongsList",
+          component: () => import("../pages/Find/components/SongsList.vue"),
+          meta: { showFooter: true },
         },
         {
-            path: "/login",
-            name: "Login",
-            component: () =>
-                import ("../pages/Login"),
-            meta: { showFooter: false },
+          path: "recommended", //每日推荐
+          name: "RecommendedDaily",
+          component: () =>
+            import("../pages/Find/components/RecommendedDaily.vue"),
+          meta: { showFooter: true },
         },
         {
-            path: "/reg",
-            name: "Reg",
-            component: () =>
-                import ("../pages/Reg"),
-            meta: { showFooter: true },
+          path: "ranklist", //排行榜
+          name: "RankList",
+          component: () => import("../pages/Find/components/RankList"),
+          meta: { showFooter: true },
         },
         {
-            path: "/find",
-            name: "Find",
-            component: () =>
-                import ("../pages/Find"),
-            meta: { showFooter: true },
-            children: [{
-                    path: "songslist", //歌单
-                    name: "SongsList",
-                    component: () =>
-                        import ("../pages/Find/components/SongsList.vue"),
-                    meta: { showFooter: true },
-                },
-                {
-                    path: "recommended", //每日推荐
-                    name: "RecommendedDaily",
-                    component: () =>
-                        import ("../pages/Find/components/RecommendedDaily.vue"),
-                    meta: { showFooter: true },
-                },
-                {
-                    path: "ranklist", //排行榜
-                    name: "RankList",
-                    component: () =>
-                        import ("../pages/Find/components/RankList"),
-                    meta: { showFooter: true },
-                },
-                {
-                    path: "privatefm", //私人FM
-                    name: "PrivateFM",
-                    component: () =>
-                        import ("../pages/Find/components/PrivateFM"),
-                    meta: { showFooter: true },
-                },
-            ],
+          path: "privatefm", //私人FM
+          name: "PrivateFM",
+          component: () => import("../pages/Find/components/PrivateFM"),
+          meta: { showFooter: true },
         },
-        {
-            path: "/blog",
-            name: "Blog",
-            component: () =>
-                import ("../pages/Blog"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/mine",
-            name: "Mine",
-            component: () =>
-                import ("../pages/Mine"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/attention",
-            name: "Attention",
-            component: () =>
-                import ("../pages/Attention"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/country",
-            name: "Country",
-            component: () =>
-                import ("../pages/Country"),
-            meta: { showFooter: true },
-        },
-        {
-            path: "/sidebar",
-            name: "Sidebar",
-            component: () =>
-                import ("../pages/Sidebar"),
-            meta: { showFooter: false },
-        },
-<<<<<<< HEAD
-    ],
-=======
       ],
+    },
+    {
+      path: "/blog",
+      name: "Blog",
+      component: () => import("../pages/Blog"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/mine",
+      name: "Mine",
+      component: () => import("../pages/Mine"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/attention",
+      name: "Attention",
+      component: () => import("../pages/Attention"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/country",
+      name: "Country",
+      component: () => import("../pages/Country"),
+      meta: { showFooter: true },
+    },
+    {
+      path: "/sidebar",
+      name: "Sidebar",
+      component: () => import("../pages/Sidebar"),
+      meta: { showFooter: false },
     },
     {
       path: "/songlist",
@@ -102,26 +88,10 @@ const router = new VueRouter({
       component: () => import("../pages/Find/components/SonglistDetail"),
     },
     {
-      path: "/blog",
-      name: "Blog",
-      component: () => import("../pages/Blog"),
-    },
-    {
-      path: "/mine",
-      name: "Mine",
-      component: () => import("../pages/Mine"),
-    },
-    {
-      path: "/attention",
-      name: "Attention",
-      component: () => import("../pages/Attention"),
-    },
-    {
-      path: "/country",
-      name: "Country",
-      component: () => import("../pages/Country"),
+      path: "/player",
+      name: "Player",
+      component: () => import("../pages/Player"),
     },
   ],
->>>>>>> 3b27de4973d5231e1451cdc382c97a5854259074
 });
 export default router;
